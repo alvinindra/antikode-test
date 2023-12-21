37f5efa3d6095b7c9633b5956559912d5d983291
Alvin Indra <alvinindrap@gmail.com>
2023-12-21 23:15:37 +0700

    feat: slicing jumbotron

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['jsx', 'js'],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    config.experiments = { ...config.experiments, topLevelAwait: true }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
--- src/components/Home/HomeJumbotron/HomeJumbotron.jsx
+++ src/components/Home/HomeJumbotron/HomeJumbotron.jsx
@@ -3,6 +3,8 @@ import styles from './HomeJumbotron.module.scss'
 import Link from 'next/link'
 import Image from 'next/image'
 import Navbar from '@/components/Base/Navbar/Navbar'
+import ICArrowNext from '@/icons/arrow/icon-next.svg'
+import ICArrowDown from '@/icons/arrow/icon-down.svg'
 
 export default function HomeJumbotron() {
   return (
@@ -12,13 +14,13 @@ export default function HomeJumbotron() {
       </div>
       <div className="container container-space">
         <div className="row">
-          <div className="col-md-5">
+          <div className="col-lg-5">
             <h1 className="text-white">
               Adopt Globally,
               <br /> Adopt Locally
             </h1>
             <p
-              className="mb-4 text-desktop-body text-white80"
+              className="mb-5 text-desktop-body text-white80"
               style={{ maxWidth: '477px' }}
             >
               Daya Dimensi Indonesia is a world-class strategic partner for
@@ -27,15 +29,67 @@ export default function HomeJumbotron() {
               hire, promote, and develop exceptional leaders.
             </p>
             <div className="d-flex">
-              <Link href={'/'} className="d-flex align-items-center">
-                <p className="mb-0 me-2 text-white">LEARN MORE ABOUT US</p>
-                <Image src={'arrow.svg'} alt="arrow" width={20} height={20} />
+              <Link href="/about" className="d-flex me-5 underline-animation">
+                <p className="mb-0 me-2 text-desktop-link text-white">
+                  LEARN MORE ABOUT US
+                </p>
+                <ICArrowNext className="my-auto" />
               </Link>
-              <Link href={'/'} className="d-flex align-items-center">
-                <p className="mb-0 me-2 text-white">CONTACT US</p>
-                <Image src={'arrow.svg'} alt="arrow" width={20} height={20} />
+              <Link href="/" className="d-flex underline-animation">
+                <p className="mb-0 me-2 text-desktop-link text-white">
+                  CONTACT US
+                </p>
+                <ICArrowNext className="my-auto" />
               </Link>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="row g-0">
+        <div className={clsx(styles.heroInfo, 'col-12 col-lg-7 d-flex')}>
+          <div className="d-flex flex-column">
+            <div className={styles.heroInfoDiscover}>Discover</div>
+            <ICArrowDown />
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4">
+                <div className={styles.heroInfoTitle}>24+ Years</div>
+                <div className="text-desktop-body text-black40 text-nowrap">
+                  Experience
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className={styles.heroInfoTitle}>50.000+</div>
+                <div className="text-desktop-body text-black40 text-nowrap">
+                  Executive Clients
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className={styles.heroInfoTitle}>400+</div>
+                <div className="text-desktop-body text-black40 text-nowrap">
+                  Organization Clients
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-lg-5 d-flex">
+          <div className="d-flex my-auto flex-row py-4">
+            <Image
+              className="ms-4 me-32px w-100"
+              src="/img/jumbotron/weps.png"
+              alt="Proud to Support Women's Empowerment Principles"
+              width={195}
+              height={64}
+            />
+            <Image
+              className="w-100"
+              src="/img/jumbotron/iso.png"
+              alt="Certified ISO"
+              width={260}
+              height={64}
+            />
           </div>
         </div>
       </div>
